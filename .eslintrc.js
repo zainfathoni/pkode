3ddeeceaa65099231c8566f4cc45a046924bb505
Zain Fathoni <me@zainf.dev>
2022-06-14 10:18:01 +0800

    build(eslint): ignore import/no-unresolved errors in exported files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   ...require('./src/config/eslintrc'),
+  overrides: [
+    {
+      files: ['babel.js', 'config.js', 'eslint.js', 'prettier.js'],
+      rules: {
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
 }
