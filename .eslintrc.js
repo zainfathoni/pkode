c28b0719cbfeb0f57b7f792752ff191a162e821b
Zain Fathoni <me@zainf.dev>
2022-06-18 14:59:23 +0800

    fix(test): lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,22 @@ module.exports = {
   ...require('./src/config/eslintrc'),
   overrides: [
     {
-      files: ['babel.js', 'config.js', 'eslint.js', 'jest.js', 'prettier.js'],
+      files: [
+        'babel.js',
+        'config.js',
+        'eslint.js',
+        'jest.js',
+        'jest.projects.js',
+        'prettier.js',
+      ],
       rules: {
         'import/no-unresolved': 'off',
+      },
+    },
+    {
+      files: ['jest.js', 'jest.projects.js'],
+      rules: {
+        'import/extensions': 'off',
       },
     },
   ],
