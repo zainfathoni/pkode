2ee63b55aaa0031c1237a826364fd9aa7cea97cb
Zain Fathoni <me@zainf.dev>
2022-06-18 14:49:04 +0800

    feat(test): expose jest project configurations

---
+++ jest.projects.js
@@ -0,0 +1,9 @@
+// https://github.com/zainfathoni/kelas.rumahberbagi.com/pull/100
+
+'use strict'
+
+module.exports = {
+  client: require('./dist/config/jest/jest.client'),
+  common: require('./dist/config/jest/jest.common'),
+  server: require('./dist/config/jest/jest.server'),
+}
